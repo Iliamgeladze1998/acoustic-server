808c77b904d60d52770cb8bbeabd4b443cf18ee5
iliamgeladze1998 <mgeladzeilia39@gmail.com>
2026-07-15 10:13:08 +0000

    Add favicon (acoustic logo) to review page

--- review-redirect/app.py
+++ review-redirect/app.py
@@ -1,10 +1,16 @@
-from flask import Flask, send_file
+from flask import Flask, send_file, send_from_directory
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return send_file('/root/review-redirect/index.html')
+
+@app.route('/favicon.ico')
+@app.route('/image.png')
+def favicon():
+    return send_from_directory('/root/review-redirect', 'image.png')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5555)
