3952b70ec1af6f13d5b54b4ce4bd85481f5e17ec
iliamgeladze1998 <mgeladzeilia39@gmail.com>
2026-07-15 10:44:23 +0000

    Add real acoustic.ge logo to header

--- review-redirect/app.py
+++ review-redirect/app.py
@@ -1,5 +1,4 @@
 from flask import Flask, send_file, send_from_directory
-import os
 
 app = Flask(__name__)
 
@@ -11,6 +10,10 @@ def index():
 @app.route('/logo_for_acoustic_browser_tab.png')
 def favicon():
     return send_from_directory('/root/review-redirect', 'logo_for_acoustic_browser_tab.png')
+
+@app.route('/acoustic_logo.png')
+def acoustic_logo():
+    return send_from_directory('/root/review-redirect', 'acoustic_logo.png')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5555)
