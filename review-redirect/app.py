c4fa9c13e3b9fcf0168f4e6eab6c30a09da4a482
iliamgeladze1998 <mgeladzeilia39@gmail.com>
2026-07-15 10:22:41 +0000

    Update dev branch with correct dev paths

--- review-redirect/app.py
+++ review-redirect/app.py
@@ -5,12 +5,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return send_file('/root/review-redirect/index.html')
+    return send_file('/root/review-redirect-dev/index.html')
 
 @app.route('/favicon.ico')
 @app.route('/logo_for_acoustic_browser_tab.png')
 def favicon():
-    return send_from_directory('/root/review-redirect', 'logo_for_acoustic_browser_tab.png')
+    return send_from_directory('/root/review-redirect-dev', 'logo_for_acoustic_browser_tab.png')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5556)
